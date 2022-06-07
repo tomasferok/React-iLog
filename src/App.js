a0843662e23f196a196ca59b488d6ffbe1d86123
Tomas <tomaasfer11@gmail.com>
2022-06-07 17:36:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import CrudProveedor from './Components/CrudProveedor';
-import HeaderComp from './Components/HeaderComp';
+
+
 
 function App() {
   return (
     <div className='container'>
-     <HeaderComp/>
+     
      <CrudProveedor/>
 
     </div>
